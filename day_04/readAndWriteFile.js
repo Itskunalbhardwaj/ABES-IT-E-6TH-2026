4f97917966b71f2b4fedf93d0f754b35c002d310
rohinimi <rohini.mittal.it.2018@miet.ac.in>
2026-02-23 10:53:55 +0530

    crud json

--- day_04/readAndWriteFile.js
+++ day_04/readAndWriteFile.js
@@ -2,6 +2,8 @@ import fs from "fs/promises";
 export const readFile = async (path) => {
     try {
         const data = await fs.readFile(path, "utf-8");
+        console.log(data);
+        
         return JSON.parse(data);
     } catch (error) {
         console.log("unable to read file");
