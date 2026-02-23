d937119cde7005976f6f16fe4c350354863ab5ae
rohinimi <rohini.mittal.it.2018@miet.ac.in>
2026-02-23 12:14:08 +0530

    controller login fun

--- day_06/controller/login.js
+++ day_06/controller/login.js
@@ -1,10 +1,18 @@
 import { readFile } from "../utils/readFile.js";
 const FILE = "../user.json";
 const userLogin = async (userDetail) => {
+    const { email, password } = userDetail;
+
     const users = await readFile(FILE);
     if (users.length === 0) {
         console.log("user is not existing");
         return;
     }
-    // const user = users.filter((u)=>u.email===)
+
+    const user = users.filter((u) => u.email === email);
+    if (user.length === 0) {
+        console.log("user is not existing");
+        return;
+    }
+
 }
