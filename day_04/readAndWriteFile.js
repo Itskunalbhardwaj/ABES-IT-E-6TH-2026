56d48918c63ee3974eef09897ecd44692e8f888d
rohinimi <rohini.mittal.it.2018@miet.ac.in>
2026-03-16 13:04:37 +0530

    htto

--- day_04/readAndWriteFile.js
+++ day_04/readAndWriteFile.js
@@ -1,9 +1,7 @@
 import fs from "fs/promises";
 export const readFile = async (path) => {
     try {
-        const data = await fs.readFile(path, "utf-8");
-        console.log(data);
-        
+        const data = await fs.readFile(path, "utf-8");        
         return JSON.parse(data);
     } catch (error) {
         console.log("unable to read file");
